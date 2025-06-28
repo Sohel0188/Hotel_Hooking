fcfaa1ab72072c264f4498e03dd8a4fc2208470e
Sohel0188 <sohelsikder.cse@gmail.com>
2025-06-29 01:08:09 +0600

    one to one

---
+++ database/migrations/2025_06_28_184433_create_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->integer('service_type')->nullable();
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('videos')->nullable();
+            $table->string('service_rules')->nullable();
+            $table->text('terms_and_conditions')->nullable();
+            $table->string('is_active')->default('1')->comment('1=active','0==inactive');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
